4be168b4f21891f8ad4f2be1d7642b688ed4c444
Nikhil Gupta <177606878+BitByNIK@users.noreply.github.com>
2025-01-22 22:29:20 +0530

    client implementation done

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
+import random
 import socket
-import os
 
 SERVER_IP = "10.194.40.15"
 SERVER_PORT = 5555
@@ -13,11 +13,28 @@ def main():
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect(server_address)
             s.sendall(ENTRY_NUMBER.encode())
-            public_key = s.recv(1024).decode()
-            public_key_parts = public_key.split(',')
-            P = int(public_key_parts[0])
-            Q = int(public_key_parts[1])
-            print(f"P = {P}, Q = {Q}")
+            dh_parameters = s.recv(1024).decode().split(',')
+            P = int(dh_parameters[0])
+            G = int(dh_parameters[1])
+            print(f"P = {P}, G = {G}")
+
+            private_key = random.getrandbits(32)
+            print(f"Private Key of {ENTRY_NUMBER}: {private_key}")
+
+            public_key = pow(G, private_key, P)
+            s.sendall(str(public_key).encode())
+            print(f"Public Key of {ENTRY_NUMBER}: {public_key}")
+
+            shared_secret = pow(int(s.recv(1024).decode()), private_key, P)
+            print(f"Shared Secret: {shared_secret}")
+
+            private_key_server = 1
+            while (True):
+                if shared_secret == pow(G, private_key * private_key_server, P):
+                    print(f"Private Key of Server: {private_key_server}")
+                    break
+                private_key_server += 1
+
     except Exception as e:
         print(f"Failed to connect to server: {e}")
 
